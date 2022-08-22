793a6afd2a516d06e252aaa44655aea1fc503a52
Karl Olson <76186248+KarlOlson@users.noreply.github.com>
2022-08-21 21:52:43 -0400

    Update Docker.py

--- seedemu/compiler/Docker.py
+++ seedemu/compiler/Docker.py
@@ -39,9 +39,10 @@ while read -sr expr; do {
 }; done
 [ "$last_pid" != 0 ] && kill $last_pid
 """
+#Optionally you can add --database.dbPath /ganache to the ganache command to make database...but not recommended.
 DockerCompilerFileTemplates['ganache'] = """\
 #!/bin/bash
-ganache -a 200 -p 8545 -h 10.100.0.100 --deterministic --database.dbPath /ganache &
+ganache -a 200 -p 8545 -h 10.100.0.100 --deterministic &
 sleep 2
 cd /bgp_smart_contracts/src 
 python3 compile.py 
@@ -242,8 +243,10 @@ fi
 DockerCompilerFileTemplates['proxy'] = """\
 #!/bin/bash
 cd /bgp_smart_contracts/src/ 
-python3 proxy.py {} &
+./wait_for_it.sh 10.100.0.100:8545 -- python3 proxy.py {} &
 echo 'Proxy setup ran. Listening for packets...'
+cd ..
+cd ..
 """
 
 DockerCompilerFileTemplates['seedemu_worker'] = """\
