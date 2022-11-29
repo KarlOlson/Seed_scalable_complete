a5deb77d345c5b63a1b2d38b2858c56cd0ca5928
Greg <gregorycusack@gmail.com>
2022-11-28 19:02:14 -0700

    trying to add advertisement to aedv contract

--- bgp_smart_contracts/src/Classes/PacketProcessing/FiveTuple.py
+++ bgp_smart_contracts/src/Classes/PacketProcessing/FiveTuple.py
@@ -10,7 +10,7 @@ def get_ip_address(ifname):
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
         0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
+        struct.pack('256s', bytes(ifname[:15], 'utf-8'))
     )[20:24])
 
 def get_interface_names():
