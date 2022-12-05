6b0746280c14b5e3556c5aa39e80accc07154d8d
Greg <gregorycusack@gmail.com>
2022-12-05 13:35:34 -0700

    update control plane. had bug

--- Hijack/control_plane.sh
+++ Hijack/control_plane.sh
@@ -2,7 +2,7 @@
 
 hijacked_route=0
 no_hijacked_route=0
-Routers=($(docker ps -f "name=router" -q))
+Routers=($(docker ps -f "name=(-router)|(-r[0-9])" -q))
 RPKI=($(docker ps -f "name=rpki" -q))
 
 hijacked_prefix=$1
@@ -18,10 +18,15 @@ for router in ${Routers[@]}; do
     string=${string#*"$delimiter"}
   done
   echo "############### ${AS_info[0]} ###############" 
-  # docker exec $router /bin/bash -c "birdc show route all | grep "74.80.186.0/25"" && let hijacked_route++ || let no_hijacked_route++ 
-  docker exec $router /bin/bash -c "birdc show route all | grep $hijacked_prefix" && let hijacked_route++ || let no_hijacked_route++ 
+  if [[ $(docker exec $router /bin/bash -c "birdc show route all | grep $hijacked_prefix") ]]; then
+    echo "Hijacked route found in ${AS_info[0]}"
+    let hijacked_route++
+  else
+    echo "No hijacked route found in ${AS_info[0]}"
+    let no_hijacked_route++
+  fi
 done
 
 echo "#Routers that have the hijacked route in its RIB: $hijacked_route"
-echo "#Routers that do not have the hijacked route in its RIB: $no_hijacked_route"
+echo "#Routers that do not have the hijacked route in its RIB: $no_hijacked_route" # need to subtract 1. always gets 1 extra router for whatever reason
 echo "#ASs deployed RPKI: $(echo ${#RPKI[*]})"
