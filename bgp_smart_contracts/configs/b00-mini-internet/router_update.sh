3f69f79b716b6e5b7b718ad325bb41502346d31d
Karl Olson <76186248+KarlOlson@users.noreply.github.com>
2023-02-04 13:49:14 -0500

    Add files via upload
    updated router configs to fix routing. some were missing bgptable.

---
+++ bgp_smart_contracts/configs/b00-mini-internet/router_update.sh
@@ -0,0 +1,200 @@
+
+asn=100
+router=$(docker container ls  | grep 'output_rs_ix_ix'$asn | awk '{print $1}')
+
+docker cp IX$asn.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn bird.conf updated"
+
+asn=101
+router=$(docker container ls  | grep 'output_rs_ix_ix'$asn | awk '{print $1}')
+
+docker cp IX$asn.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn bird.conf updated"
+
+
+asn=102
+router=$(docker container ls  | grep 'output_rs_ix_ix'$asn | awk '{print $1}')
+
+docker cp IX$asn.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn bird.conf updated"
+
+asn=103
+router=$(docker container ls  | grep 'output_rs_ix_ix'$asn | awk '{print $1}')
+
+docker cp IX$asn.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn bird.conf updated"
+
+asn=104
+router=$(docker container ls  | grep 'output_rs_ix_ix'$asn | awk '{print $1}')
+
+docker cp IX$asn.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn bird.conf updated"
+
+asn=105
+router=$(docker container ls  | grep 'output_rs_ix_ix'$asn | awk '{print $1}')
+
+docker cp IX$asn.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn bird.conf updated"
+
+
+asn=2
+router_num=101
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=3
+router_num=103
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=12
+router_num=101
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=2
+router_num=100
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=2
+router_num=102
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=2
+router_num=105
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=3
+router_num=100
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=3
+router_num=104
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=3
+router_num=105
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=4
+router_num=100
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=4
+router_num=102
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=4
+router_num=104
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=11
+router_num=102
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=11
+router_num=105
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
+
+asn=12
+router_num=104
+router=$(docker container ls  | grep 'output_rnode_'$asn'_r'$router_num | awk '{print $1}')
+echo $router
+
+docker cp $asn-r$router_num.conf $router:/etc/bird/bird.conf
+docker exec $router /bin/bash -c "birdc configure"
+
+echo "ASN$asn router: $asn"_r"$router_num bird.conf updated"
